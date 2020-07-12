740a461c73b7fb13d641845d0c0a4bb1dbc8b42b
Adnin <adninzannatul1@gmail.com>
2020-07-12 01:55:23 -0400

    Controlled Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -50,7 +50,7 @@ class Main extends Component {
               <Route path='/home' component={ HomePage } />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/aboutus' component={() => <About dishes={this.state.dishes} />} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Route path='/contactus' component={ Contact } />
               <Redirect to="/home" />
           </Switch>
